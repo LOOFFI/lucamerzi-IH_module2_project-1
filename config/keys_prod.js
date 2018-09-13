091e001d050e844cb9fbaf49a49eec8489aacfd5
LOOFFI <aurelien.rosaz@gmail.com>
2018-09-13 15:11:51 +0200

    d

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: process.env.MONGOLAB_PINK_URI,
+  mongoURI: process.env.MONGODB_URI,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 }
