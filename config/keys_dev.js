f1d356cb8e522eef050e39168084d018b2bf17cc
LOOFFI <aurelien.rosaz@gmail.com>
2019-12-09 14:59:56 +0100

    keys changes

--- config/keys_dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	// mongoURI: 'mongodb://localhost/project2',
-	// mongoURI: 'mongodb://heroku_0thmj8s8:lmoivbd6rcrstib6bqk4tugudj@ds155492.mlab.com:55492/heroku_0thmj8s8',
-	mongoURI: 'mongodb://localhost/project2',
-	googleClientID: "1040645846951-id98rvu32857bpp7nrvmok2e5utnu46a.apps.googleusercontent.com",
-	googleClientSecret: "KQqM0_grRZI9BNrLzitu5VHs",
-}
-
-// ONCE ADDED NEW HARD-CODED TOKENS, ADD .keys_dev TO .gitignore
